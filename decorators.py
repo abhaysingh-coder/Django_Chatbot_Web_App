fde9041043bb59b3893d95e7966ea730f0043211
abhaysingh-coder <srinetabhaysingh14@gmail.com>
2026-05-09 23:43:17 +0530

    Update login_required_role decorator to redirect to mainapp:Loginhome

--- decorators.py
+++ decorators.py
@@ -4,10 +4,10 @@ def login_required_role(required_role):
     def decorator(view_func):
         def wrapper(request, *args, **kwargs):
             if not request.session.get('email'):
-                return redirect('login')
+                return redirect('mainapp:Loginhome')
 
             if request.session.get('role') != required_role:
-                return redirect('login')
+                return redirect('mainapp:Loginhome')
 
             return view_func(request, *args, **kwargs)
         return wrapper
