a269da9fe513eaf3612ffab41492d38860c5f0ce
abhaysingh-coder <srinetabhaysingh14@gmail.com>
2026-05-10 01:35:19 +0530

    Refactor model loading and prediction functions for improved caching and clarity

--- chatbot.py
+++ chatbot.py
@@ -3,17 +3,15 @@ import os
 
 model_name = ['Flags', 'Category', 'Intent']
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model = {}
-for i in model_name:
-    model_path = os.path.join(BASE_DIR, 'Model', f'{i}.pkl')
-    model_path = os.path.abspath(model_path)
-    with open(model_path, 'rb') as f:
-        model[i] = pickle.load(f)
+
+model_cache = {}
+def load_model(model_name):
+    if model_name not in model_cache:
+        model_path = os.path.join(BASE_DIR, 'Model', f'{model_name}.pkl')
+        with open(model_path, 'rb') as f:
+            model_cache[model_name] = pickle.load(f)
+    return model_cache[model_name]
 
 def prediction(text, model_name):
-    if model_name == 'Flags':
-        return model['Flags'].predict([text])[0]
-    elif model_name == 'Category':
-        return model['Category'].predict([text])[0]
-    elif model_name == 'Intent':
-        return model['Intent'].predict([text])[0]
+    model = load_model(model_name)
+    return model.predict([text])[0]
