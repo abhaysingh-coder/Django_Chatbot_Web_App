477f815ab83b8a0bb26fd5b1b1fa0e8081c4c3a1
abhaysingh-coder <srinetabhaysingh14@gmail.com>
2026-05-10 15:37:58 +0530

    Update ChatMessage model fields for improved data handling and consistency

--- Customer/models.py
+++ Customer/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
-# Create your models here.
 class ChatMessage(models.Model):
     Role = models.CharField(max_length=20)
     Email = models.CharField(max_length=100)
-    Flags = models.CharField(max_length=10)
+    Flags = models.CharField(max_length=100)
+    Category = models.CharField(max_length=100)
+    Intent = models.CharField(max_length=100)
     Utterance = models.TextField()
-    Category = models.CharField(max_length=10)
-    Intent = models.CharField(max_length=10)
+
     def __str__(self):
         return self.Email
+
 
 class ChatHistory(models.Model):
     Role = models.CharField(max_length=20)
@@ -19,4 +20,4 @@ class ChatHistory(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.username} - {self.timestamp}"
+        return f"{self.Email} - {self.timestamp}"
